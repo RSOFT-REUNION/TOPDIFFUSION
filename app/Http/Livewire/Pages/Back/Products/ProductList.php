e67bc3d44e3b97a2b997f9d5d2bf3e84dba7eb83
BrunoRsoft <139841836+BrunoRsoft@users.noreply.github.com>
2023-09-08 11:18:17 +0400

    BO | Recherche produit :white_check_mark:

--- app/Http/Livewire/Pages/Back/Products/ProductList.php
+++ app/Http/Livewire/Pages/Back/Products/ProductList.php
@@ -2,16 +2,85 @@
 
 namespace App\Http\Livewire\Pages\Back\Products;
 
-use App\Models\MyProduct;
 use App\Models\Product;
 use Livewire\Component;
+use App\Models\MyProduct;
+use App\Models\MyProductPromotion;
 
 class ProductList extends Component
 {
+
+    public $search = '';
+
+    public $perPage = 30;
+
+    public $clear;
+
+    public $filters_count = 0;
+
+    public $jobs = [];
+
+    public $is_search = false;
+
+    public function updatedSearch()
+    {
+        $query = '%' . $this->search . '%';
+
+        if (auth()->user()->admin == 1) {
+            if (strlen($this->search) > 2) {
+                $this->jobs = MyProduct::where('title', 'like', $query)
+                    // ->orWhere('due_date', 'like', $query)
+                    // ->orWhere('code_client', 'like', $query)
+                    // ->orWhere('balance_of', 'like', $query)
+                    ->get();
+                $this->is_search = true;
+            } else if ($this->is_search) {
+                $this->is_search = false;
+                $this->jobs = [];
+                MyProduct::all();
+            }
+        } else {
+            if (strlen($this->search) > 2) {
+                $this->jobs = MyProduct::where('title', 'like', $query)
+                    // ->where('code_client', auth()->user()->code)
+                    // ->orWhere('MyProductPromotions.due_date', 'like', $query)
+                    // ->orWhere('MyProductPromotions.num_facture', 'like', $query)
+                    ->get();
+                $this->is_search = true;
+            } else if ($this->is_search) {
+                $this->is_search = false;
+                $this->jobs = [];
+                MyProduct::all();
+            }
+        }
+    }
+
+    public function clear()
+    {
+        $this->search = '';
+        $this->is_search = false;
+        $this->jobs = [];
+    }
     public function render()
     {
         $data = [];
         $data['products'] = MyProduct::orderBy('created_at', 'desc')->get();
+
+        if ($this->jobs) {
+            $products = $this->jobs;
+        } else {
+            if (auth()->user()->team == 1) {
+                $products = MyProduct::query()->orderBy('title')->orderBy('slug')->orderBy('cover');
+            } else {
+                $products = MyProduct::query()->orderBy('title');
+            }
+        }
+
+        if ($this->jobs) {
+            $data['products'] = $products;
+        } else {
+            $data['products'] = $products->paginate($this->perPage);
+        }
         return view('livewire.pages.back.products.product-list', $data);
     }
 }
--- resources/views/livewire/pages/back/products/product-list.blade.php
+++ resources/views/livewire/pages/back/products/product-list.blade.php
@@ -5,6 +5,12 @@
         </div>
         <div class="flex-none inline-flex items-center">
             <a href="" class="btn-icon mr-2"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <input wire:model.debounce.100ms="search" type="text" placeholder="Rechercher un produit..." class="bg-transparent px-2 py-3 focus:outline-none border-none focus:border-none duration-300 hover:tracking-wider dark:border-none">
+            @if ($search)
+                <button wire:click="clear" class="px-3">
+                    <i class="fa-solid fa-times"></i>
+                </button>
+            @endif
             <a href="" class="btn-icon mr-2"><i class="fa-solid fa-file-import"></i></a>
             <a href="{{ route('back.product.create') }}" class="btn-secondary cursor-pointer"><i class="fa-solid fa-plus mr-3"></i>Ajouter un produit</a>
         </div>
