50c7017bac9d59810c1c3d81dc74bd30ee5d94da
ADELINE-RSOFT <139841836+BrunoRsoft@users.noreply.github.com>
2023-09-12 08:52:11 +0400

    BO : marques en cours

--- app/Http/Livewire/Pages/Back/Products/Brands.php
+++ app/Http/Livewire/Pages/Back/Products/Brands.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Livewire\Pages\Back\Products;
 
+use App\Models\MyProduct;
 use App\Models\ProductBrand;
 use Livewire\Component;
 
 class Brands extends Component
 {
+
+    public function deleteBrand($id)
+    {
+        $brand = ProductBrand::find($id);
+
+        if ($brand) {
+            MyProduct::where('brand_id', $brand->id)->update(['brand_id' => 2]);
+
+            $brand->delete();
+        }
+
+        return redirect()->route('back.product.brands');
+    }
+
+
     public function render()
     {
         $data = [];
