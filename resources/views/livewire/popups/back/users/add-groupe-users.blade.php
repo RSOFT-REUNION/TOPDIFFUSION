236e567669e795983abfd796a537573711f67ce7
Bruno <139841836+BrunoRsoft@users.noreply.github.com>
2023-10-02 20:55:43 +0400

    Update add-groupe-users.blade.php

--- resources/views/livewire/popups/back/users/add-groupe-users.blade.php
+++ resources/views/livewire/popups/back/users/add-groupe-users.blade.php
@@ -15,29 +15,42 @@
         <form wire:submit.prevent="createGroupUser">
             @csrf
             <div class="textfield">
-                <label for="name">Nom de Groupe Utilisateurs <span class="text-red-500">*</span></label>
+                <label for="name">Nom du groupe d'utilisateurs <span class="text-red-500">*</span></label>
                 <input type="text" id="name" wire:model="name" placeholder="Entrez un nom de groupe"
-                    class="@if ($errors->has('name')) input-error @endif" value="{{ old('name') }}">
+                       class="@if ($errors->has('name')) input-error @endif" value="{{ old('name') }}">
             </div>
 
-            {{-- ! CE QUI EST COMMENTE PAS BESOIN POUR LE MOMENT A LAISSE AU CAS OU --}}
-            {{-- <div class="textfield mt-2">
+            <div class="textfield mt-2">
                 <label for="discount_percentage">Pourcentage de remise (%) <span class="text-red-500">*</span></label>
                 <input type="text" wire:model="discount_percentage" id="discount_percentage"
-                    placeholder="Entrez une remise en pourcentage"
-                    class="@if ($errors->has('discount_percentage')) input-error @endif" pattern="[0-9]+(\.[0-9]+)?" min="0"
-                    max="90" value="{{ old('discount_percentage') }}" />
-            </div> --}}
-            {{-- <label for="isDefault">Groupe par défaut :</label>
-            <input type="checkbox" wire:model="isDefault" id="isDefault"> --}}
+                       placeholder="Entrez la remise pour ce groupe"
+                       class="@if ($errors->has('discount_percentage')) input-error @endif" pattern="[0-9]+(\.[0-9]+)?" min="0"
+                       max="90" required value="{{ old('discount_percentage') }}" />
+                @error('discount_percentage')
+                <span class="text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="btn-check-line flex items-center mt-2">
+                <div class="flex-1">
+                    <label for="is_default">Remise par défaut :</label>
+                    <input type="checkbox" wire:model="is_default" id="is_default">
+                    <p>Si "Remise par défaut" est coché toutes les catégories qui n'ont pas été configuré manuellement auront <span class="font-bold">{{ $discount_percentage }} %</span> de remise.</p>
+                </div>
+            </div>
 
             <div class="mt-4">
                 <label>Sélectionnez les utilisateurs à ajouter au groupe :</label>
-                <select id="userSelect" wire:model="selectedUsers" multiple class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" size="5">
+                <div class="btn-check-line flex items-center mt-2">
+                    <p>Les utilisateurs mis dans un groupe ne pourront pas être ajoutés à un autre groupe.</p>
+                </div>
+                <div class="space-y-2 mt-2">
                     @foreach ($usersList as $user)
-                        <option value="{{ $user->id }}">{{ $user->lastname }} {{ $user->firstname }}</option>
+                        <label class="inline-flex items-center">
+                            <input id="userSelect" type="checkbox" wire:model="selectedUsers" value="{{ $user->id }}" class="form-checkbox h-5 w-5 text-blue-600">
+                            <span class="ml-2">{{ $user->lastname }} {{ $user->firstname }}</span>
+                        </label>
                     @endforeach
-                </select>
+                </div>
             </div>
             <div class="mt-10">
                 <button type="submit" class="btn-secondary block w-full">Ajouter</button>
@@ -45,22 +58,23 @@
         </form>
 
     </div>
+    <script>
+        const userSelect = document.getElementById('userSelect');
+
+        userSelect.addEventListener('click', function(event) {
+            const option = event.target;
+            if (option.tagName === 'OPTION' && !option.selected) {
+                option.selected = true;
+            }
+        });
+
+        userSelect.addEventListener('dblclick', function(event) {
+            const option = event.target;
+            if (option.tagName === 'OPTION') {
+                option.selected = false;
+            }
+        });
+    </script>
 </div>
 
-<script>
-    const userSelect = document.getElementById('userSelect');
 
-    userSelect.addEventListener('click', function(event) {
-        const option = event.target;
-        if (option.tagName === 'OPTION' && !option.selected) {
-            option.selected = true;
-        }
-    });
-
-    userSelect.addEventListener('dblclick', function(event) {
-        const option = event.target;
-        if (option.tagName === 'OPTION') {
-            option.selected = false;
-        }
-    });
-</script>
