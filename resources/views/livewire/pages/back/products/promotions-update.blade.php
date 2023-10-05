fd6f079c169a085f67ac769c6f7e4b6c5e8cb826
devAlexandreRsoft <dev@rsoft.re>
2023-10-05 16:47:21 +0400

    Update promotions-update.blade.php

--- resources/views/livewire/pages/back/products/promotions-update.blade.php
+++ resources/views/livewire/pages/back/products/promotions-update.blade.php
@@ -13,7 +13,7 @@
                 <a class="hover:bg-red-100 duration-500 ease-out px-4 py-3 rounded-full mr-7 cursor-pointer" wire:click="delete">
                     <i class="fa-solid fa-trash"></i>
                 </a>
-                <button type="submit" class="btn-secondary flex gap-x-4 items-center"><i class="fa-solid fa-square-plus text-lg"></i>Publier ma promotion</button>
+                <button type="submit" class="btn-secondary flex gap-x-4 items-center"><i class="fa-solid fa-square-plus text-lg"></i>Mettre a jours ma promotion</button>
             </div>
         </div>
 
