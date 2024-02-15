814061ab3e246f2da84a6d146dbd880493376101
HOARAU BRIAN <brianhoaraunery@gmail.com>
2024-02-15 14:37:57 +0100

    Gestion des stocks
    - ⏳ Popups d'affichage des stocks + Mise à jour des stocks

--- app/Models/MyProductStock.php
+++ app/Models/MyProductStock.php
@@ -23,6 +23,15 @@ class MyProductStock extends Model
         return MyProductSwatch::where('product_id', $this->product_id)->first();
     }
 
+    public function getBadge()
+    {
+        if($this->quantity > 3) {
+            return '<span class="bg-blue-200 text-blue-600 rounded-md text-sm py-1 px-2">En stock</span>';
+        } else {
+            return '<span class="bg-orange-200 text-orange-600 rounded-md text-sm py-1 px-2">Stock faible</span>';
+        }
+    }
+
     public function getupdatedDate()
     {
         if (strtotime($this->updated_at) == strtotime(Carbon::now())) {
