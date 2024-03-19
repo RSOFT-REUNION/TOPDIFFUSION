d2f4cf612a9e0dc6d478d3e8be3f614fe414c30c
DevRsoft (RSOFT) <dev@rsoft.re>
2024-03-19 09:42:47 +0400

    ajout du favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <title>Top diffusion</title>
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon"/>
     <!-- METAS -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
