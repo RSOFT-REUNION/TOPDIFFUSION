7647419ff07686e82da05630f9be8448098b6400
DevRsoft (RSOFT) <dev@rsoft.re>
2024-04-18 14:26:57 +0400

    cacher dashobard pour la prod

--- app/Http/Livewire/Pages/Back/Dashboard.php
+++ app/Http/Livewire/Pages/Back/Dashboard.php
@@ -18,104 +18,104 @@ class Dashboard extends Component
     public $sales, $productCreated, $newAccountCreated, $productMoreSold, $averagePurchase, $totalSalesRevenue;
 
     protected $activityLog = [];
-    public function mount()
-    {
-        $this->sales = $this->salesPerWeek();
-        $this->productCreated = $this->productCreateThisMonth();
-        $this->newAccountCreated = $this->newAccountCreatedThisMonth();
-        $this->productMoreSold = $this->productMoreSold();
-        $this->activityLog = $this->getActivityLog();
-        $this->averagePurchase = $this->averagePurchase();
-        $this->totalSalesRevenue = $this->salesRevenue();
-        $this->monthlyAverage = $this->averagePurchase();
-    }
-    public function salesPerWeek()
-    {
-        // Obtenez les ventes pour la semaine actuelle
-        $currentWeekData = UserOrderItem::selectRaw('COUNT(*) as total_sales')
-            ->whereRaw('DATE_FORMAT(created_at, "%Y-%U") = DATE_FORMAT(NOW(), "%Y-%U")')
-            ->first();
+//     public function mount()
+//     {
+//         $this->sales = $this->salesPerWeek();
+//         $this->productCreated = $this->productCreateThisMonth();
+//         $this->newAccountCreated = $this->newAccountCreatedThisMonth();
+//         $this->productMoreSold = $this->productMoreSold();
+//         $this->activityLog = $this->getActivityLog();
+//         $this->averagePurchase = $this->averagePurchase();
+//         $this->totalSalesRevenue = $this->salesRevenue();
+//         $this->monthlyAverage = $this->averagePurchase();
+//     }
+//     public function salesPerWeek()
+//     {
+//         // Obtenez les ventes pour la semaine actuelle
+//         $currentWeekData = UserOrderItem::selectRaw('COUNT(*) as total_sales')
+//             ->whereRaw('DATE_FORMAT(created_at, "%Y-%U") = DATE_FORMAT(NOW(), "%Y-%U")')
+//             ->first();
 
-        return $currentWeekData;
-    }
+//         return $currentWeekData;
+//     }
 
-    public function productCreateThisMonth()
-    {
-        // Obtenir le nombre de produits créés ce mois-ci
-        $currentMonthData = MyProduct::whereMonth('created_at', Carbon::now()->month)
-            ->count();
+//     public function productCreateThisMonth()
+//     {
+//         // Obtenir le nombre de produits créés ce mois-ci
+//         $currentMonthData = MyProduct::whereMonth('created_at', Carbon::now()->month)
+//             ->count();
 
-        return $currentMonthData;
-    }
+//         return $currentMonthData;
+//     }
 
-    public function newAccountCreatedThisMonth ()
-    {
-        // Obtenir le nombre de nouveaux comptes créer ce mois-ci
-        $newAccount = User::selectRaw('COUNT(*) as total_new_account')
-            ->whereRaw('DATE_FORMAT(created_at, "%Y-%m") = DATE_FORMAT(NOW(), "%Y-%m")')
-            ->first();
+//     public function newAccountCreatedThisMonth ()
+//     {
+//         // Obtenir le nombre de nouveaux comptes créer ce mois-ci
+//         $newAccount = User::selectRaw('COUNT(*) as total_new_account')
+//             ->whereRaw('DATE_FORMAT(created_at, "%Y-%m") = DATE_FORMAT(NOW(), "%Y-%m")')
+//             ->first();
 
-        return $newAccount;
-    }
+//         return $newAccount;
+//     }
 
-    public function productMoreSold()
-    {
-        // Obtenir le produit le plus vendu ce mois-ci
-        $productMoreSold = UserOrderItem::selectRaw('my_products.title as product_name, COUNT(*) as total_sales')
-            ->join('my_products', 'user_order_items.product_id', '=', 'my_products.id')
-            ->whereRaw('DATE_FORMAT(user_order_items.created_at, "%Y-%m") = DATE_FORMAT(NOW(), "%Y-%m")')
-            ->groupBy('user_order_items.product_id')
-            ->orderByDesc('total_sales')
-            ->first();
+//     public function productMoreSold()
+//     {
+//         // Obtenir le produit le plus vendu ce mois-ci
+//         $productMoreSold = UserOrderItem::selectRaw('my_products.title as product_name, COUNT(*) as total_sales')
+//             ->join('my_products', 'user_order_items.product_id', '=', 'my_products.id')
+//             ->whereRaw('DATE_FORMAT(user_order_items.created_at, "%Y-%m") = DATE_FORMAT(NOW(), "%Y-%m")')
+//             ->groupBy('user_order_items.product_id')
+//             ->orderByDesc('total_sales')
+//             ->first();
 
-//        dd($productMoreSold);
-        return $productMoreSold;
-    }
+// //        dd($productMoreSold);
+//         return $productMoreSold;
+//     }
 
-    public function averagePurchase()
-    {
-        // Date il y a 7 jours à partir d'aujourd'hui
-        $sevenDaysAgo = Carbon::now()->subDays(7);
+//     public function averagePurchase()
+//     {
+//         // Date il y a 7 jours à partir d'aujourd'hui
+//         $sevenDaysAgo = Carbon::now()->subDays(7);
 
-        // Obtenir la somme des montants des commandes pour les 7 derniers jours
-        $totalSalesRevenue = UserOrder::where('created_at', '>=', $sevenDaysAgo)
-            ->sum('total_amount');
+//         // Obtenir la somme des montants des commandes pour les 7 derniers jours
+//         $totalSalesRevenue = UserOrder::where('created_at', '>=', $sevenDaysAgo)
+//             ->sum('total_amount');
 
-        // Obtenir le nombre de clients uniques ayant effectué des achats au cours de la période
-        $uniqueCustomerCount = UserOrder::where('created_at', '>=', $sevenDaysAgo)
-            ->distinct('user_id')
-            ->count('user_id');
+//         // Obtenir le nombre de clients uniques ayant effectué des achats au cours de la période
+//         $uniqueCustomerCount = UserOrder::where('created_at', '>=', $sevenDaysAgo)
+//             ->distinct('user_id')
+//             ->count('user_id');
 
 
-        // Calculer le panier moyen en fonction du nombre de clients
-        if ($uniqueCustomerCount > 0) {
-            $averagePurchase = $totalSalesRevenue / $uniqueCustomerCount;
-        } else {
-            $averagePurchase = null; // Valeur par défaut ici
-        }
+//         // Calculer le panier moyen en fonction du nombre de clients
+//         if ($uniqueCustomerCount > 0) {
+//             $averagePurchase = $totalSalesRevenue / $uniqueCustomerCount;
+//         } else {
+//             $averagePurchase = null; // Valeur par défaut ici
+//         }
 
-        // Formater le résultat avec une virgule comme séparateur décimal
-        if ($averagePurchase !== null) {
-            $averagePurchase = number_format($averagePurchase, 2, ',', '.');
-        }
+//         // Formater le résultat avec une virgule comme séparateur décimal
+//         if ($averagePurchase !== null) {
+//             $averagePurchase = number_format($averagePurchase, 2, ',', '.');
+//         }
 
-        return $averagePurchase;
-    }
+//         return $averagePurchase;
+//     }
 
-    public function salesRevenue()
-    {
-        // Date il y a 7 jours à partir d'aujourd'hui
-        $sevenDaysAgo = Carbon::now()->subDays(7);
+//     public function salesRevenue()
+//     {
+//         // Date il y a 7 jours à partir d'aujourd'hui
+//         $sevenDaysAgo = Carbon::now()->subDays(7);
 
-        // Obtenir la somme des montants des commandes pour les 7 derniers jours
-        $totalSalesRevenue = UserOrderItem::where('created_at', '>=', $sevenDaysAgo)
-            ->sum(DB::raw('quantity * product_price'));
+//         // Obtenir la somme des montants des commandes pour les 7 derniers jours
+//         $totalSalesRevenue = UserOrderItem::where('created_at', '>=', $sevenDaysAgo)
+//             ->sum(DB::raw('quantity * product_price'));
 
-        // Formater le chiffre d'affaires réalisé avec une virgule comme séparateur décimal
-        $totalSalesRevenue = number_format($totalSalesRevenue, 2, ',', '.');
+//         // Formater le chiffre d'affaires réalisé avec une virgule comme séparateur décimal
+//         $totalSalesRevenue = number_format($totalSalesRevenue, 2, ',', '.');
 
-        return $totalSalesRevenue;
-    }
+//         return $totalSalesRevenue;
+//     }
 
 
 
