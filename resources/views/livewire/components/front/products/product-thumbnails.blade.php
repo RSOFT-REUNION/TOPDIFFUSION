955fdbab1ec076d60443a63a7db7aa9da73b24ee
DevRsoft (RSOFT) <dev@rsoft.re>
2024-04-18 14:15:32 +0400

    Revert "Merge branch 'Actualisations' into nettoyage"
    This reverts commit 0d5479757c832df5ff0020065b242066ef11d682, reversing
    changes made to c934565959deecc36b49106f9171e77fe93e2a02.

--- resources/views/livewire/components/front/products/product-thumbnails.blade.php
+++ resources/views/livewire/components/front/products/product-thumbnails.blade.php
@@ -1,34 +1,44 @@
-<div class="relative bg-slate-100 rounded-xl border overflow-hidden group hover:drop-shadow-2xl hover:scale-105 duration-300 hover:bg-white" role="button" data-href="{{ route('front.product', ['slug' => $product->slug]) }}">
+<div class="item-thumbnail" role="button" data-href="{{ route('front.product', ['slug' => $product->slug]) }}">
     <div class="flex flex-col h-full">
-        <div class="flex-1 p-2">
-            <div class="flex">
-                <div class="flex-none">
-                    <div class="absolute top-5 left-0 group-hover:left-5 duration-300">
-                        @if($product_stock == 0)
-                            <p class="my-1 bg-red-400 font-bold py-2 px-5 rounded-r-full duration-300 group-hover:bg-white group-hover:border group-hover:border-red-400 group-hover:rounded-l-full group-hover:text-red-400">Rupture de stock</p>
-                        @elseif($product_stock < 3 && $product_stock > 0)
-                            <p class="my-1 bg-orange-400 font-bold py-2 px-5 rounded-r-full duration-300 group-hover:bg-white group-hover:border group-hover:border-orange-400 group-hover:rounded-l-full group-hover:text-orange-400">Plus que {{ $product_stock }} en stock</p>
-                        @endif
-                        @if ($promotion)
-                            <p class="my-1 bg-amber-400 font-bold py-2 px-5 rounded-r-full duration-300 group-hover:bg-white group-hover:border group-hover:border-amber-400 group-hover:rounded-l-full group-hover:text-amber-400">Remise de {{ $this->promotion->discount }} %</p>
-                        @endif
+        <div class="flex-1">
+            <div class="thunbnail-icons">
+                <div class="content-icons">
+                    <div class="flex">
+                        <div class="flex-none">
+                            <div class="tags-group flex gap-x-3 ml-1">
+                                @if($product_stock == 0)
+                                    <p class="tags bg-red-500 text-white">Rupture de stock</p>
+                                @elseif($product_stock < 3 && $product_stock > 0)
+                                    <p class="tags bg-orange-500 text-white">Plus que {{ $product_stock }}</p>
+                                @endif
+                                @if ($promotion)
+                                    <p class="tags bg-[#fbbc34] text-black">{{ $this->promotion->discount }} %</p>
+                                @endif
+                            </div>
+                        </div>
+                        {{-- <div class="flex-grow"></div> --}}
+                        <div class="flex-none absolute right-0 top-3">
+                            @if($isFavorite)
+                                <a class="py-[14px] px-[16px] rounded-full duration-300 bg-[#ff253a20] hover:bg-gray-200 hover:text-black text-red-500" wire:click.stop="deleteFavorite({{ $product->id }})  "><i class="fa-solid fa-heart"></i></a>
+                            @else
+                                <a class="py-[14px] px-[16px] rounded-full duration-300  bg-gray-200 hover:text-red-500 hover:bg-[#ff253a20]" wire:click.stop="addProductToFavorite({{ $product->id }}) "><i class="fa-solid fa-heart"></i></a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-                {{-- <div class="flex-grow"></div> --}}
-                <div class="flex-none absolute right-5 top-10">
-                    @if($isFavorite)
-                        <a class="py-[14px] px-[16px] rounded-full duration-300 bg-[#ff253a20] hover:bg-gray-200 hover:text-black text-red-500" wire:click.stop="deleteFavorite({{ $product->id }})  "><i class="fa-solid fa-heart"></i></a>
-                    @else
-                        <a class="py-[14px] px-[16px] rounded-full duration-300  bg-gray-200 hover:text-red-500 hover:bg-[#ff253a20]" wire:click.stop="addProductToFavorite({{ $product->id }}) "><i class="fa-solid fa-heart"></i></a>
-                    @endif
-                </div>
             </div>
-            <div class="force-center w-full">
+            <div class="force-center mb-5 w-full">
                 <img class="rounded-lg w-full" src="{{ asset('storage/images/products/'. $product->cover) }}">
             </div>
         </div>
-        <div class="flex-none p-5 relative">
-            <h2 class="font-bold text-xl text-slate-500">{{ $product->title }}</h2>
+        <div class="flex-none">
+            <div class="inline-flex">
+                {{-- <h4 class="marque-tag">{{ $product->getBrand()->title }}</h4> --}}
+                @if($product->type == 1)
+                    <h4 class="ml-1 other-tag">UGS: {{ $product->getUgs() }}</h4>
+                @endif
+            </div>
+            <h2 class="mt-2">{{ $product->title }}</h2>
             <div class="inline-flex prices">
                 @if(!auth()->guest() && auth()->user()->professionnal === 1 && auth()->user()->verified === 1 && $my_setting->professionnal_prices === 1)
                     {{-- Si le produit est en promotion et que l'option mes tarifs est activée --}}
@@ -42,7 +52,7 @@
                             <p class="text-sm text-gray-400 line-through">{{ number_format($product->getPriceWithDiscount(), 2, ',', ' ') }} €</p>
                         </div>
                     @else
-                        <h3 class="text-[30px] font-semibold">{{ number_format($product->getPriceWithDiscount(), '2', ',', ' ') }} €</h3>
+                        <h3>{{ number_format($product->getPriceWithDiscount(), '2', ',', ' ') }} €</h3>
                         @if(number_format($product->getCustomerDiscount(), '0', ',', ' ') != 0)
                             <p class="text-sm text-gray-400">HT (-{{ number_format($product->getCustomerDiscount(), '0', ',', ' ') }} %)</p>
                         @endif
@@ -60,30 +70,26 @@
                         </div>
                         <p class="text-sm text-gray-400">TTC</p>
                     @else
-                        <h3 class="text-[30px] font-semibold">{{ number_format($product->getPriceTTC(), '2', ',', ' ') }} €</h3>
-                        <p class="text-sm text-gray-400 ml-2">TTC</p>
+                        <h3>{{ number_format($product->getPriceTTC(), '2', ',', ' ') }} €</h3>
+                        <p class="text-sm text-gray-400">TTC</p>
                     @endif
                 @endif
             </div>
-            <div class="absolute right-10 bottom-10 invisible group-hover:visible">
-                <i class="fa-solid fa-arrow-right fa-2x text-slate-300"></i>
-            </div>
-        </div>
-        @if(!auth()->guest() && auth()->user()->professionnal === 1 && auth()->user()->verified === 1 && $setting->prices_type === 1 && $my_setting->professionnal_prices === 1)
-            <div class="flex-none border-t bg-amber-100 p-5">
-
+            @if(!auth()->guest() && auth()->user()->professionnal === 1 && auth()->user()->verified === 1 && $setting->prices_type === 1 && $my_setting->professionnal_prices === 1)
                 <div class="public-price">
                     <div class="flex items-center">
                         <div class="flex-1">
-                            <p class="text-amber-600">Prix public conseillé</p>
+                            <p>Prix public conseillé</p>
                         </div>
                         <div class="flex-none">
-                            <h3 class="font-bold text-lg">{{ number_format($product->getPriceTTC(), '2', ',', ' ') }} € <span class="opacity-20">(TTC)</span></h3>
+                            <h3>{{ number_format($product->getPriceTTC(), '2', ',', ' ') }} € (TTC)</h3>
                         </div>
                     </div>
                 </div>
-
+            @endif
+            <div class="mt-3">
+                <a href="{{ route('front.product', ['slug' => $product->slug]) }}" class="btn-secondary block text-center">En savoir plus</a>
             </div>
-        @endif
+        </div>
     </div>
 </div>
